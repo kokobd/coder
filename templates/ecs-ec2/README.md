de49878633432b74a07705ea3019fb97f194b7c3
kokobd <contact@zelinf.net>
2023-11-06 17:58:18 -0500

    wip infra for ec2+ecs

---
+++ templates/ecs-ec2/README.md
@@ -0,0 +1,6 @@
+# ecs-ec2
+
+EC2 instance backed ECS containers.
+
+Each workspace created using this template runs on ECS, on a dedicated EC2
+instance.
---
+++ templates/ecs-ec2/main.tf
@@ -0,0 +1,209 @@
+terraform {
+  required_providers {
+    coder = {
+      source = "coder/coder"
+    }
+    aws = {
+      source = "hashicorp/aws"
+    }
+  }
+}
+
+data "coder_provisioner" "me" {
+}
+
+data "coder_workspace" "me" {
+}
+
+
+data "coder_parameter" "dotfiles_uri" {
+  name         = "dotfiles_uri"
+  display_name = "dotfiles URI"
+  description  = <<-EOF
+  Dotfiles repo URI (optional)
+
+  see https://dotfiles.github.io
+  EOF
+  default      = "https://github.com/${data.coder_workspace.me.owner}/dotfiles"
+  type         = "string"
+  mutable      = true
+}
+
+
+data "coder_parameter" "docker_image" {
+  name         = "docker_image"
+  display_name = "Docker image"
+  default      = "zelinf/coder-nix:latest"
+}
+
+data "coder_parameter" "cpu_count" {
+  name         = "cpu_count"
+  display_name = "CPU count"
+  type         = "number"
+  default      = 2
+}
+
+data "coder_parameter" "memory_gib" {
+  name         = "memory_gib"
+  display_name = "Memory in GiB"
+  type         = "number"
+  default      = 8
+}
+
+resource "coder_agent" "main" {
+  arch                    = data.coder_provisioner.me.arch
+  os                      = "linux"
+  startup_script_timeout  = 180
+  env                     = { "DOTFILES_URI" = data.coder_parameter.dotfiles_uri.value != "" ? data.coder_parameter.dotfiles_uri.value : null }
+  dir                     = "/workspace"
+  startup_script_behavior = "blocking"
+  startup_script          = <<-EOT
+    set -e
+    if [ -n "$DOTFILES_URI" ]; then
+      echo "Installing dotfiles from $DOTFILES_URI"
+      coder dotfiles -y "$DOTFILES_URI"
+    fi
+    mkdir -p /workspace
+    sudo chown -R coder:coder /workspace
+  EOT
+}
+
+resource "aws_ecs_service" "main" {
+  name                = "coder-${data.coder_workspace.me.id}"
+  desired_count       = data.coder_workspace.me.start_count
+  launch_type         = "EC2"
+  scheduling_strategy = "DAEMON"
+  placement_constraints {
+    type       = "memberOf"
+    expression = "attribute:coder.workspace_id == ${data.coder_workspace.me.id}"
+  }
+  task_definition = aws_ecs_task_definition.main.arn
+  network_configuration {
+    subnets         = [for subnet in data.aws_subnets.main.ids : subnet]
+    security_groups = [data.aws_security_group.main.id]
+  }
+  cluster = aws_ecs_cluster.main.arn
+}
+
+resource "aws_ecs_cluster" "main" {
+  name = "coder-${data.coder_workspace.me.id}"
+}
+
+resource "aws_ecs_task_definition" "main" {
+  family = "coder-${data.coder_workspace.me.id}"
+  container_definitions = jsonencode([
+    {
+      name      = "main"
+      image     = data.coder_parameter.docker_image.value
+      essential = true
+      # TODO add EFS volume
+    }
+  ])
+  network_mode             = "awsvpc"
+  requires_compatibilities = ["EC2"]
+  memory                   = data.coder_parameter.memory_gib.value * 1024
+  # TODO add EFS volume
+}
+
+resource "aws_ec2_fleet" "main" {
+  timeouts {
+    delete = "30m"
+  }
+  launch_template_config {
+    launch_template_specification {
+      launch_template_id = aws_launch_template.main.id
+      version            = aws_launch_template.main.latest_version
+    }
+    dynamic "override" {
+      for_each = data.aws_subnets.main.ids
+      content {
+        subnet_id = override.value
+      }
+    }
+  }
+  target_capacity_specification {
+    default_target_capacity_type = "spot"
+    total_target_capacity        = data.coder_workspace.me.start_count
+  }
+  type                = "maintain"
+  terminate_instances = true
+}
+
+resource "aws_launch_template" "main" {
+  block_device_mappings {
+    device_name = "/dev/xvda"
+
+    ebs {
+      delete_on_termination = true
+      iops                  = 3000
+      throughput            = 125
+      volume_size           = 100
+      volume_type           = "gp3"
+    }
+  }
+  update_default_version               = true
+  description                          = "Coder ECS"
+  image_id                             = data.aws_ami.amazon_linux_2023_ecs.id
+  instance_initiated_shutdown_behavior = "terminate"
+  instance_requirements {
+    allowed_instance_types = ["m7i.*", "r7a.*", "m6i.*", "r6a.*"]
+
+    vcpu_count {
+      min = data.coder_parameter.cpu_count.value
+    }
+    memory_mib {
+      min = data.coder_parameter.memory_gib.value * 1024
+    }
+  }
+  key_name = "macbook air"
+  user_data = base64encode(<<EOF
+#!/bin/bash
+    mkdir -p /etc/ecs
+    echo 'ECS_CLUSTER=${aws_ecs_cluster.main.name}' >> /etc/ecs/ecs.config
+    echo 'ECS_INSTANCE_ATTRIBUTES={"coder.workspace_id":${data.coder_workspace.me.id}}' >> /etc/ecs/ecs.config
+  EOF
+  )
+  network_interfaces {
+    associate_public_ip_address = true
+    device_index                = 0
+    delete_on_termination       = true
+    security_groups             = [data.aws_security_group.main.id]
+  }
+  iam_instance_profile {
+    arn = aws_iam_instance_profile.main.arn
+  }
+}
+
+resource "aws_iam_instance_profile" "main" {
+  role = "AmazonEC2ContainerServiceforEC2Role"
+}
+
+data "aws_security_group" "main" {
+  tags = {
+    coder = "true"
+  }
+}
+
+data "aws_subnets" "main" {
+  filter {
+    name   = "vpc-id"
+    values = [data.aws_security_group.main.vpc_id]
+  }
+}
+
+data "aws_ami" "amazon_linux_2023_ecs" {
+  owners      = ["591542846629"]
+  most_recent = true
+  filter {
+    name   = "name"
+    values = ["al2023-ami-ecs*"]
+  }
+  filter {
+    name   = "architecture"
+    values = ["x86_64"]
+  }
+  filter {
+    name   = "virtualization-type"
+    values = ["hvm"]
+  }
+}
